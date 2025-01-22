254a7f1bd2f50422f02fbd5bab5e60c7beebcd60
Madan Pattanshetty <madanpattanashetty4@gmail.com>
2025-01-22 16:35:45 +0530

    POST end point

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
+
+let skills = ['Basketball', 'Badminton', 'Swimming', 'Drawing'];
 
 app.get('/api/skills', (req, res) => {
     res.json({
         success: true,
         message: 'Skills retrieved successfully',
-        skills: ['Basketball', 'Badminton', 'Swimming', 'Drawing']
+        skills: skills,
+    });
+});
+
+// POST endpoint
+app.post('/api/skills', (req, res) => {
+    const { skill } = req.body;
+
+    if (!skill) {
+        return res.status(400).json({
+            success: false,
+            message: 'Skill is required',
+        });
+    }
+
+    skills.push(skill);
+    res.status(201).json({
+        success: true,
+        message: 'Skill added successfully',
+        skills: skills,
     });
 });
 
